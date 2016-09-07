e221a2961fc4aec8bee4d74751831a0989657928
Elliot Wright <wright.elliot@gmail.com>
2016-09-07 15:09:39 +0100

    Made build + push script actually work.

--- build-push.sh
+++ build-push.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
-PWD=$(pwd)
+set -ex
 
-cd "${PWD}/5.6/"
+WORKDIR=$(pwd)
+
+cd "${WORKDIR}/5.6/"
 docker build -t seeruk/php-fpm:5.6 .
 
-cd "${PWD}/5.6-dev/"
+cd "${WORKDIR}/5.6-dev/"
 docker build -t seeruk/php-fpm:5.6-dev .
 
-cd "${PWD}/7.0-dev/"
+cd "${WORKDIR}/7.0-dev/"
 docker build -t seeruk/php-fpm:7.0-dev .
 
-cd "${PWD}"
+cd "${WORKDIR}"
 docker push seeruk/php-fpm:5.6
 docker push seeruk/php-fpm:5.6-dev
 docker push seeruk/php-fpm:7.0-dev
