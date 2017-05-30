2cf91aef5c6f9696f93f639447ecad3fd9649cb0
Elliot Wright <wright.elliot@gmail.com>
2017-05-30 14:25:20 +0100

    Added 7.1-dev image.

--- build-push.sh
+++ build-push.sh
@@ -13,7 +13,11 @@ docker build -t seeruk/php-fpm:5.6-dev .
 cd "${WORKDIR}/7.0-dev/"
 docker build -t seeruk/php-fpm:7.0-dev .
 
+cd "${WORKDIR}/7.1-dev/"
+docker build -t seeruk/php-fpm:7.1-dev .
+
 cd "${WORKDIR}"
 docker push seeruk/php-fpm:5.6
 docker push seeruk/php-fpm:5.6-dev
 docker push seeruk/php-fpm:7.0-dev
+docker push seeruk/php-fpm:7.1-dev
