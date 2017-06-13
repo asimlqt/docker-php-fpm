37f23ad4ef97233137783550afaea1078b340f98
Elliot Wright <wright.elliot@gmail.com>
2017-06-13 12:11:50 +0100

    Don't use cache for building.

--- build-push.sh
+++ build-push.sh
@@ -5,16 +5,16 @@ set -ex
 WORKDIR=$(pwd)
 
 cd "${WORKDIR}/5.6/"
-docker build -t seeruk/php-fpm:5.6 .
+docker build --no-cache -t seeruk/php-fpm:5.6 .
 
 cd "${WORKDIR}/5.6-dev/"
-docker build -t seeruk/php-fpm:5.6-dev .
+docker build --no-cache -t seeruk/php-fpm:5.6-dev .
 
 cd "${WORKDIR}/7.0-dev/"
-docker build -t seeruk/php-fpm:7.0-dev .
+docker build --no-cache -t seeruk/php-fpm:7.0-dev .
 
 cd "${WORKDIR}/7.1-dev/"
-docker build -t seeruk/php-fpm:7.1-dev .
+docker build --no-cache -t seeruk/php-fpm:7.1-dev .
 
 cd "${WORKDIR}"
 docker push seeruk/php-fpm:5.6
